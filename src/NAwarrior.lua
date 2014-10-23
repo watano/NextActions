1e6e01fed9b9fc6d5241a0ba8608dcdac57588f5
watano <watano@qq.com>
2014-10-23 12:29:53 +0800

    优化防战和狂暴战配置,增加自动回血和援护等技能支持

--- src/NAwarrior.lua
+++ src/NAwarrior.lua
@@ -3,11 +3,11 @@ function getNA1Actions(no)
     return {};
   elseif(no == 0)then
     return {
-      '355','5308','6572','78','23922','20243','100','34428','55694','6673'
+      '355','5308','6572','78','23922','20243','156321','100','57755','107570','114029','34428','112048','12975','871','1160','6673'
     };
   elseif(no == 1)then
     return {
-      '5308','23881','100130','85288','34428','100','1680','103840'
+      '5308','23881','107570','100130','85288','34428','100','57755','46924','1680'
     };
   elseif(no == 2)then
     return {
@@ -39,9 +39,11 @@ function NA1Dps()
       -- 保命施法
     
       if(false
-          or NA_Fire(NA_ProfileNo == 1 and W_HPlevel(NA_Player)<0.7, '34428', NA_Player) --乘胜追击
-or NA_Fire(NA_ProfileNo == 1 and W_HPlevel(NA_Player)<0.5, '55694', NA_Player) --狂怒回复
-or NA_Fire(NA_IsSolo and W_HPlevel(NA_Player)<0.2, '103840', NA_Player) --胜利在望
+          or NA_Fire(not NA_IsMaxDps and W_HPlevel(NA_Player)<0.8, '34428', NA_Player) --乘胜追击
+or NA_Fire(NA_ProfileNo == 0 and W_HPlevel(NA_Player)<0.8, '112048', NA_Player) --盾牌屏障
+or NA_Fire(NA_ProfileNo == 0 and W_HPlevel(NA_Player)<0.2, '12975', NA_Player) --破釜沉舟
+or NA_Fire(NA_ProfileNo == 0 and W_HPlevel(NA_Player)<0.2, '871', NA_Player) --盾墙
+or NA_Fire(NA_ProfileNo == 0 and W_HPlevel(NA_Player)<0.1, '1160', NA_Player) --挫志怒吼
  )then return true; end
 
       if(NA_ProfileNo < 0)then
@@ -52,12 +54,15 @@ or NA_Fire(NA_IsSolo and W_HPlevel(NA_Player)<0.2, '103840', NA_Player) --胜利
       
       if(not NA_IsAOE and (false
         or NA_Fire(notTanking, '355', NA_Target) --嘲讽
-or NA_Fire(W_HPlevel(NA_Target)<0.2, '5308', NA_Target) --斩杀
+or NA_Fire(true, '5308', NA_Target) --斩杀
 or NA_Fire(W_PowerLevel(NA_Player) < 0.5, '6572', NA_Target) --复仇
 or NA_Fire(true, '78', NA_Target) --英勇打击
 or NA_Fire(true, '23922', NA_Target) --盾牌猛击
 or NA_Fire(W_BuffCount(NA_Target, -113746, true)<3, '20243', NA_Target) --毁灭打击
+or NA_Fire(true, '156321', NA_Target) --盾牌冲锋
 or NA_Fire(true, '100', NA_Target) --冲锋
+or NA_Fire(true, '57755', NA_Target) --英勇投掷
+or NA_Fire(true, '107570', NA_Target) --风暴之锤
 
       ))then return true; end
 
@@ -66,7 +71,10 @@ or NA_Fire(true, '100', NA_Target) --冲锋
 or NA_Fire(W_PowerLevel(NA_Player) < 0.5, '6572', NA_Target) --复仇
 or NA_Fire(true, '23922', NA_Target) --盾牌猛击
 or NA_Fire(W_BuffCount(NA_Target, -113746, true)<3, '20243', NA_Target) --毁灭打击
+or NA_Fire(true, '156321', NA_Target) --盾牌冲锋
 or NA_Fire(true, '100', NA_Target) --冲锋
+or NA_Fire(true, '57755', NA_Target) --英勇投掷
+or NA_Fire(true, '107570', NA_Target) --风暴之锤
 
       ))then return true; end
     elseif(NA_ProfileNo == 1)then --Fury
@@ -74,22 +82,28 @@ or NA_Fire(true, '100', NA_Target) --冲锋
       
       if(not NA_IsAOE and (false
         or NA_Fire(true, '5308', NA_Target) --斩杀
-or NA_Fire(true, '23881', NA_Target) --嗜血
+or NA_Fire(W_PowerLevel(NA_Player) < 0.7, '23881', NA_Target) --嗜血
+or NA_Fire(NA_IsMaxDps, '107570', NA_Target) --风暴之锤
 or NA_Fire(W_HasBuff(NA_Player, 46916, true), '100130', NA_Target) --狂风打击
 or NA_Fire(W_HPlevel(NA_Target)>0.2 and W_HasBuff(NA_Player, 131116, true), '85288', NA_Target) --怒击
 or NA_Fire(W_HasBuff(NA_Player, 32216, true), '34428', NA_Target) --乘胜追击
-or NA_Fire(true, '100130', NA_Target) --狂风打击
 or NA_Fire(W_PowerLevel(NA_Player) > 0.4, '85288', NA_Target) --怒击
+or NA_Fire(true, '100130', NA_Target) --狂风打击
 or NA_Fire(true, '100', NA_Target) --冲锋
+or NA_Fire(true, '57755', NA_Target) --英勇投掷
+or NA_Fire(NA_IsSolo, '107570', NA_Target) --风暴之锤
 
       ))then return true; end
 
       if(NA_IsAOE and (false
-        or NA_Fire(W_HPlevel(NA_Target)<0.2, '5308', NA_Target) --斩杀
+        or NA_Fire(true, '5308', NA_Target) --斩杀
+or NA_Fire(NA_IsMaxDps, '46924', NA_Target) --剑刃风暴
 or NA_Fire(true, '1680', NA_Target) --旋风斩
-or NA_Fire(true, '23881', NA_Target) --嗜血
+or NA_Fire(W_PowerLevel(NA_Player) < 0.7, '23881', NA_Target) --嗜血
 or NA_Fire(true, '100130', NA_Target) --狂风打击
 or NA_Fire(true, '100', NA_Target) --冲锋
+or NA_Fire(true, '57755', NA_Target) --英勇投掷
+or NA_Fire(true, '107570', NA_Target) --风暴之锤
 
       ))then return true; end
     elseif(NA_ProfileNo == 2)then --Arms
@@ -106,13 +120,16 @@ or NA_Fire(true, '100', NA_Target) --冲锋
       end
     elseif(UnitCanAssist(NA_Player, NA_Target) and UnitIsPlayer(NA_Target))then
       if(false
-              )then return true; end
+        or NA_Fire(not NA_IsSolo, '114029', NA_Target) --捍卫
+      )then return true; end
       return false;
     end
   else
     
     if(false
       or NA_Fire(NA_IsSolo and W_TargetCanAttack(), '100', NA_Target) --冲锋
+or NA_Fire(NA_IsSolo and NA_ProfileNo == 0 and W_TargetCanAttack(), '156321', NA_Target) --盾牌冲锋
+or NA_Fire(NA_IsSolo and W_TargetCanAttack(), '57755', NA_Target) --英勇投掷
 or NA_Fire(NA_IsSolo and not W_HasBuff(NA_Player, 6673, true), '6673', NA_Player) --战斗怒吼
     )then return true; end
   end
