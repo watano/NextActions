b052a7f6d1d8a45da6b265f93b6d91678a975c76
watano <watano@qq.com>
2016-07-22 15:40:43 +0800

    update spell info

--- codegen/fetchAll.bat
+++ codegen/fetchAll.bat
@@ -1,5 +1,5 @@
 call jset.bat
 e:\work\devtools\dart-sdk\bin\dart.exe WowInfo.dart
+e:\work\devtools\dart-sdk\bin\dart.exe WowReader.dart
 jsfmt -j -w 1.json 2.json 3.json  4.json  5.json  6.json  7.json  8.json  9.json  10.json  11.json 
-
 pause
