3cfe9f336938d893ca958b7a21a7458bf202d406
watano <watano@qq.com>
2016-07-29 17:32:48 +0800

    update for Fury

--- src/NAwarrior.lua
+++ src/NAwarrior.lua
@@ -3,7 +3,7 @@ function getNA1Actions(no)
   elseif(no == 0)then
     return {};
   elseif(no == 1)then
-    return {'5308','23881','85288','184367','190411','205545','118000','46968','107570','100130','100','57755'};
+    return {'184364','97462','5308','1719','118000','184367','85288','23881','190411','100','57755'};
   elseif(no == 2)then
     return {};
   end
@@ -11,7 +11,7 @@ function getNA1Actions(no)
 end
 
 NA1ProfileNames = {[0]='防战',[1]='狂暴战',[2]='武器战',};
-NA1ProfileDescriptions = {[0]='天赋:--属性:',[1]='天赋:1112311--属性:',[2]='天赋:--属性:',};
+NA1ProfileDescriptions = {[0]='天赋:--属性:',[1]='天赋:2313333--属性:急速>暴击>精通/全能',[2]='天赋:--属性:',};
 
 function NA1Dps()
   W_Log(1,"战士 dps");
@@ -59,20 +59,23 @@ function NA1Dps()
         local hascs = W_RetainBuff(NA_Player, 29725, true);   --猝死
 				local hasjn = W_RetainBuff(NA_Player, 13046, true);   --激怒
 				local hascklx =  W_HasBuff(NA_Player, 6673, true); --摧枯拉朽
+				local in5308 = W_HPlevel(NA_Target)<0.2; --斩杀阶段
 				
 				
         
         if(not NA_IsAOE and (false
+					or NA_Fire(NA_IsSolo and NA_checkHP(2), '184364', NA_Player) --狂怒回复
+					or NA_Fire(NA_IsSolo and NA_checkHP(0), '97462', NA_Player) --命令怒吼
 					or NA_Fire(true, '5308', NA_Target) --斩杀
-					or NA_Fire(true, '23881', NA_Target) --嗜血
+					or NA_Fire(true, '1719', NA_Player) --战吼
+					or NA_Fire(true, '118000', NA_Player) --巨龙怒吼
+					or NA_Fire(in5308 and W_PowerLevel(NA_Player) > 0.85, '184367', NA_Target) --暴怒
+					or NA_Fire(in5308, '85288', NA_Target) --怒击
+					or NA_Fire(in5308, '23881', NA_Target) --嗜血
+					or NA_Fire(true, '184367', NA_Target) --暴怒
 					or NA_Fire(true, '85288', NA_Target) --怒击
-					or NA_Fire(W_PowerLevel(NA_Player) > 0.85, '184367', NA_Target) --暴怒
-					or NA_Fire(W_PowerLevel(NA_Player) < 0.85 and hascklx, '190411', NA_Target) --旋风斩
-					or NA_Fire(true, '205545', NA_Target) --奥丁之怒
-					or NA_Fire(NA_isUsableTalentSpell(7,3), '118000', NA_Target) --巨龙怒吼
-					or NA_Fire(NA_isUsableTalentSpell(2,1), '46968', NA_Target) --震荡波
-					or NA_Fire(NA_isUsableTalentSpell(2,2), '107570', NA_Target) --风暴之锤
-					or NA_Fire(true, '100130', NA_Target) --狂暴挥砍
+					or NA_Fire(true, '23881', NA_Target) --嗜血
+					or NA_Fire(true, '190411', NA_Target) --旋风斩
 					or NA_Fire(true, '100', NA_Target) --冲锋
 					or NA_Fire(true, '57755', NA_Target) --英勇投掷
 
@@ -80,18 +83,6 @@ function NA1Dps()
         ))then return true; end
 
         if(NA_IsAOE and (false
-					or NA_Fire(true, '5308', NA_Target) --斩杀
-					or NA_Fire(true, '23881', NA_Target) --嗜血
-					or NA_Fire(true, '85288', NA_Target) --怒击
-					or NA_Fire(W_PowerLevel(NA_Player) > 0.85, '184367', NA_Target) --暴怒
-					or NA_Fire(W_PowerLevel(NA_Player) < 0.85 and hascklx, '190411', NA_Target) --旋风斩
-					or NA_Fire(true, '205545', NA_Target) --奥丁之怒
-					or NA_Fire(NA_isUsableTalentSpell(7,3), '118000', NA_Target) --巨龙怒吼
-					or NA_Fire(NA_isUsableTalentSpell(2,1), '46968', NA_Target) --震荡波
-					or NA_Fire(NA_isUsableTalentSpell(2,2), '107570', NA_Target) --风暴之锤
-					or NA_Fire(true, '100130', NA_Target) --狂暴挥砍
-					or NA_Fire(true, '100', NA_Target) --冲锋
-					or NA_Fire(true, '57755', NA_Target) --英勇投掷
 
           or NA_fireByOvale()
         ))then return true; end
