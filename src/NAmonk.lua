a431e7aa3c83dc2cd5c828c0b8f59e50bd3f94d6
watano <watano@qq.com>
2016-08-29 22:15:08 +0800

    add monk2.ini

--- src/NAmonk.lua
+++ src/NAmonk.lua
@@ -5,12 +5,12 @@ function getNA10Actions(no)
   elseif(no == 1)then
     return {};
   elseif(no == 2)then
-    return {};
+    return {'107428','100784','100780'};
   end
   return {};
 end
 
-NA10ProfileNames = {[0]='Brewmaster',[1]='Mistweaver',[2]='Battledancer',};
+NA10ProfileNames = {[0]='酒仙',[1]='织雾',[2]='踏风',};
 NA10ProfileDescriptions = {[0]='天赋:--属性:',[1]='天赋:--属性:',[2]='天赋:--属性:',};
 
 function NA10Dps()
@@ -21,17 +21,17 @@ function NA10Dps()
 	
   if(W_IsInCombat())then
     if(NA_ProfileNo < 0)then return false; --保命施法
-    elseif(NA_ProfileNo == 0)then --Brewmaster
+    elseif(NA_ProfileNo == 0)then --酒仙
       
       if(false
 
       )then return true; end
-    elseif(NA_ProfileNo == 1)then --Mistweaver
+    elseif(NA_ProfileNo == 1)then --织雾
       
       if(false
 
       )then return true; end
-    elseif(NA_ProfileNo == 2)then --Battledancer
+    elseif(NA_ProfileNo == 2)then --踏风
       
       if(false
 
@@ -40,7 +40,7 @@ function NA10Dps()
     end
     if(W_TargetCanAttack()) then  --攻击施法
       if(NA_ProfileNo < 0)then return false;
-      elseif(NA_ProfileNo == 0)then --Brewmaster
+      elseif(NA_ProfileNo == 0)then --酒仙
         
 				
         
@@ -53,7 +53,7 @@ function NA10Dps()
 
           or NA_fireByOvale()
         ))then return true; end
-      elseif(NA_ProfileNo == 1)then --Mistweaver
+      elseif(NA_ProfileNo == 1)then --织雾
         
 				
         
@@ -66,11 +66,14 @@ function NA10Dps()
 
           or NA_fireByOvale()
         ))then return true; end
-      elseif(NA_ProfileNo == 2)then --Battledancer
+      elseif(NA_ProfileNo == 2)then --踏风
         
 				
         
         if(not NA_IsAOE and (false
+					or NA_Fire(true, '107428', NA_Target) --旭日东升踢
+					or NA_Fire(true, '100784', NA_Target) --幻灭踢
+					or NA_Fire(true, '100780', NA_Target) --猛虎掌
 
           or NA_fireByOvale()
         ))then return true; end
@@ -82,19 +85,19 @@ function NA10Dps()
       end
     elseif(UnitCanAssist(NA_Player, NA_Target))then --辅助施法
       if(NA_ProfileNo < 0)then return false;
-      elseif(NA_ProfileNo == 0)then --Brewmaster
+      elseif(NA_ProfileNo == 0)then --酒仙
         
 				
         if(false
 
         )then return true; end
-      elseif(NA_ProfileNo == 1)then --Mistweaver
+      elseif(NA_ProfileNo == 1)then --织雾
         
 				
         if(false
 
         )then return true; end
-      elseif(NA_ProfileNo == 2)then --Battledancer
+      elseif(NA_ProfileNo == 2)then --踏风
         
 				
         if(false
@@ -104,19 +107,20 @@ function NA10Dps()
     end
   else  --不在战斗中
     if(NA_ProfileNo < 0)then return false; --脱战后补buff，开怪等
-    elseif(NA_ProfileNo == 0)then --Brewmaster
+    elseif(NA_ProfileNo == 0)then --酒仙
       
       if(false
 
       )then return true; end
-    elseif(NA_ProfileNo == 1)then --Mistweaver
+    elseif(NA_ProfileNo == 1)then --织雾
       
       if(false
 
       )then return true; end
-    elseif(NA_ProfileNo == 2)then --Battledancer
+    elseif(NA_ProfileNo == 2)then --踏风
       
       if(false
+					or NA_Fire(NA_IsSolo and W_TargetCanAttack(), '100780', NA_Target) --猛虎掌
 
       )then return true; end
     end
